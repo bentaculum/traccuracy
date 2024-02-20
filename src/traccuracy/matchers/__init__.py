c5b927f001383594c5c8b403be64d61357af3ff1
Benjamin Gallusser <bgallusser@googlemail.com>
2024-02-20 19:12:49 +0100

    Add one-to-one IoU Matcher

--- src/traccuracy/matchers/__init__.py
+++ src/traccuracy/matchers/__init__.py
@@ -25,8 +25,10 @@ and edges (gt_1, gt_2) and (pred_1, pred_2) exist, they are also considered matc
 While we specify ground truth and prediction, it is possible to
 write a matching function that matches two arbitrary tracking solutions.
 """
+
 from ._compute_overlap import get_labels_with_overlap
 from ._ctc import CTCMatched
 from ._iou import IOUMatched
+from ._iou_one_to_one import IOUOneToOneMatched
 
-__all__ = ["CTCMatched", "IOUMatched", "get_labels_with_overlap"]
+__all__ = ["CTCMatched", "IOUMatched", "IOUOneToOneMatched", "get_labels_with_overlap"]
